633df28bdf1468c591217ee7729a47e0138519f6
unknown <ujjwal060>
2025-08-08 13:15:30 +0530

    add change

--- models/reserveModel.js
+++ models/reserveModel.js
@@ -71,7 +71,7 @@ const reserveSchema = mongoose.Schema(
     },
     bookingType: {
       type: String,
-      enum: ["Reservation", "Booking"],
+      enum: ["Reservation", "Booking"," Both"],
       required: true,
     },
     invoiceId: { type: Number },
